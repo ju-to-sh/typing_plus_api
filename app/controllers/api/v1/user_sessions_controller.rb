f8189011c9163b99f5697e376e7281a5838398e0
ju-to-sh <088t3748@gmail.com>
2024-06-11 07:17:31 +0900

    ユーザー登録処理を実装

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -3,10 +3,10 @@ module Api
     class UserSessionsController  < BaseController
 
       def create
-        user = login(params[:email], params[:password])
-        raise ActiveRecord::RecordNotFound unless user
+        @user = login(params[:email], params[:password])
+        raise ActiveRecord::RecordNotFound unless @user
 
-        json_string = UserSerializer.new(user).serializable_hash.to_json
+        json_string = UserSerializer.new(@user).serializable_hash.to_json
         render json: json_string
       end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get 'login' => 'user_sessions#new', :as => :login
       post 'login' => "user_sessions#create"
       post 'logout' => 'user_sessions#destroy', :as => :logout
+      resource :registration, only: %i[create]
     end
   end
 end
