6755b210dc4aa185979a11cfe310e2a6f2bbcc88
ju-to-sh <088t3748@gmail.com>
2024-06-12 05:16:52 +0900

    認証機能の実装

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -1,12 +1,14 @@
 module Api
   module V1
     class UserSessionsController  < BaseController
+      skip_before_action :authenticate
 
       def create
         @user = login(params[:email], params[:password])
         raise ActiveRecord::RecordNotFound unless @user
 
         json_string = UserSerializer.new(@user).serializable_hash.to_json
+        set_access_token!(@user)
         render json: json_string
       end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_032340) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_231712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "access_token", null: false
+    t.datetime "expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["access_token"], name: "index_api_keys_on_access_token", unique: true
+    t.index ["user_id"], name: "index_api_keys_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_032340) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "api_keys", "users"
 end
