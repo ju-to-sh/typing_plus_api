ef6724bf15f9efe54cadfec3aa9f64ea239d9057
ju-to-sh <088t3748@gmail.com>
2024-06-18 13:08:41 +0900

    corsの設定変更およびログインユーザー情報を返すメソッドを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,10 @@
 module Api
   module V1
     class UsersController < BaseController
-      def index
-        users = User.all
-        json_string = UserSerializer.new(users).serializable_hash.to_json
+
+      def show
+        user = current_user
+        json_string = UserSerializer.new(user).serializable_hash.to_json
         render json: json_string
       end
     end
