ea7dca68f37ea2f0d4ce522f551e510ed3a11643
ju-to-sh <088t3748@gmail.com>
2024-06-19 07:42:46 +0900

    マイページ編集用のapi作成

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,26 @@
 module Api
   module V1
     class UsersController < BaseController
-
       def show
         user = current_user
         json_string = UserSerializer.new(user).serializable_hash.to_json
         render json: json_string
+      end
+
+      def update
+        user = current_user
+        if user.update(user_params)
+          json_string = UserSerializer.new(user).serializable_hash.to_json
+          render json: json_string
+        else
+          render render_400(nil, user.errors.full_messages)
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:nickname, :email, :avatar)
       end
     end
   end
