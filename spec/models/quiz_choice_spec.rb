aef1ad6f75a112b4127fcecbfbe7bf343af0730a
ju-to-sh <088t3748@gmail.com>
2024-07-19 06:08:11 +0900

    quiz_choiceモデルのバリデーションテストを追加

--- spec/models/quiz_choice_spec.rb
+++ spec/models/quiz_choice_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe QuizChoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    let!(:game_list) { create(:game_list) }
+    let(:quiz) { game_list.quizzes.create(content: '問題1') }
+
+    it '設定した全てのバリデーションが機能しているか' do
+      quiz_choice = quiz.quiz_choices.new(content: '問題1の選択肢', is_correct: false)
+      expect(quiz).to be_valid
+      expect(quiz.errors).to be_empty
+    end
+
+    it 'contentがない場合にバリデーションが機能してinvalidになるか' do
+      quiz_choice = quiz.quiz_choices.new(content: '', is_correct: false)
+      expect(quiz_choice).to be_invalid
+      expect(quiz_choice.errors.messages[:content]).to eq(["can't be blank"])
+    end
+
+    it 'is_correctがtrueもしくはfalse場合にバリデーションが機能してinvalidになるか' do
+      quiz_choice = quiz.quiz_choices.new(content: '問題1の選択肢', is_correct: nil)
+      expect(quiz_choice).to be_invalid
+      expect(quiz_choice.errors.messages[:is_correct]).to eq(["is not included in the list"])
+    end
+  end
 end
